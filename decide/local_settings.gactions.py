c8c5b672ee38bd41438a19e953f60d5aff93d542
mrivas10 <manolo.rivas.llamas@gmail.com>
2022-10-11 16:51:25 +0200

    cambio

---
+++ decide/local_settings.gactions.py
@@ -0,0 +1,29 @@
+ALLOWED_HOSTS = ["*"]
+
+# Modules in use, commented modules that you won't use
+MODULES = [
+    'authentication',
+    'base',
+    'booth',
+    'census',
+    'mixnet',
+    'postproc',
+    'store',
+    'visualizer',
+    'voting',
+]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'decide',
+        'USER': 'decide',
+	'PASSWORD': 'decide',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+
+# number of bits for the key, all auths should use the same number of bits
+KEYBITS = 256
